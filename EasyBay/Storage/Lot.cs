9b93fd39667e4a547a1b7b7dec79c54716cab98d
alex <alexsamail@mail.ru>
2019-03-18 21:14:37 +0300

    Added models.

---
+++ EasyBay/Storage/Lot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Storage
+{
+    public class Lot
+    {
+        public int Id { get; set; }
+        public DateTime TradeFinishTime { get; set; }
+        public int BuyOutPrice { get; set; }
+        public int CurrentPrice { get; set; }
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoPath { get; set; }
+
+        public int? CurrentBuyerId { get; set; }
+        public virtual User CurrentBuyer { get; set; }
+
+        public int OwnerId { get; set; }
+        [InverseProperty("LotsForSale")]
+        public virtual User Owner { get; set; }
+        public List<Tag> Tags { get; set; }
+        public Lot()
+        {
+            Tags = new List<Tag>();
+        }
+    }
+}
---
+++ EasyBay/Storage/User.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Storage
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public decimal Balance { get; set; }
+        public decimal LockedBalance { get; set; }
+        [NotMapped]
+        public decimal FreeBalance => Balance - LockedBalance;
+        public string Role { get; set; }
+
+        [InverseProperty("Owner")]
+        public List<Lot> LotsForSale { get; set; }
+        public List<Lot> TrackedLots { get; set; }
+        public List<Lot> BoughtLots { get; set; }
+
+        public User()
+        {
+            LotsForSale = new List<Lot>();
+            TrackedLots = new List<Lot>();
+            BoughtLots = new List<Lot>();
+        }
+    }
+
+    public static class Role
+    {
+        public static string Admin = "Admin";
+        public static string Moderator = "Moderator";
+        public static string Seller = "Seller";
+        public static string User = "User";
+    }
+}
