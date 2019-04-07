798b55b9839e6d5d25e9a538a7e5ced4280e1864
alex <alexsamail@mail.ru>
2019-04-07 23:01:43 +0300

    Added templates for tests.

---
+++ EasyBay/EasyBay.Tests/DBTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasyBay.Tests
+{
+    [TestClass]
+    public class DBTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
