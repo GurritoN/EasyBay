c46f9232317b477fbfa8909da4f029ceb8c0268c
alex <alexsamail@mail.ru>
2019-05-14 16:09:02 +0300

    Added Raise Price Page.

---
+++ EasyBay/EasyBay/ViewModels/RaisePriceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyBay.ViewModels
+{
+    public class RaisePriceModel
+    {
+        [Required(ErrorMessage = "Не указана цена")]
+        public string Price { get; set; }
+    }
+}
