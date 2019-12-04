20e5e279f50cd66d22304def4eec53ec3ea0c8cd
Alex <alexsamail@mail.ru>
2019-12-04 08:28:37 +0300

    ..

---
+++ EasyBay/EasyBay/ViewModels/AddLotViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyBay.ViewModels
+{
+    public class AddLotViewModel
+    {
+        public DateTime TradeFinishTime { get; set; }
+        public decimal BuyOutPrice { get; set; }
+        public decimal StartingPrice { get; set; }
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
